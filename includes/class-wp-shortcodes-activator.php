89438b466157d5af06edf7e9a3bc4b695c5f79f1
mimo <mimocontact@gmail.com>
2015-10-27 17:58:04 +0100

    Init

---
+++ includes/class-wp-shortcodes-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://mimo.media
+ * @since      1.0.0
+ *
+ * @package    Wp_Shortcodes
+ * @subpackage Wp_Shortcodes/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Shortcodes
+ * @subpackage Wp_Shortcodes/includes
+ * @author     Mimo <mail@mimo.media>
+ */
+class Wp_Shortcodes_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-shortcodes-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://mimo.media
+ * @since      1.0.0
+ *
+ * @package    Wp_Shortcodes
+ * @subpackage Wp_Shortcodes/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Shortcodes
+ * @subpackage Wp_Shortcodes/includes
+ * @author     Mimo <mail@mimo.media>
+ */
+class Wp_Shortcodes_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
